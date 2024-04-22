0553b35ab0f94750dcfaa42a520eefc9739a71d7
sortA0329 <tsouta0329@gmail.com>
2024-04-22 16:12:26 +0900

    update

--- src/extension.ts
+++ src/extension.ts
@@ -2,60 +2,78 @@ import * as vscode from 'vscode';
 import * as child_process from 'node:child_process';
 import * as path from 'node:path';
 import * as https from 'node:https';
+import { check_py_version } from './checker';
 
-const py_version = [3, 12];
-const oj_version = [11, 5, 1];
-function check_py_version() {
-	return new Promise((resolve) => {
-		child_process.exec('pip3 --version', (error, stdout, stderr) => {
-			if(error) {
-				vscode.window.showErrorMessage('Please install python.');
-				resolve(false);
-				return;
-			}
-			const version = stdout.toString().replace(/.*python (\d+)\.(\d+)\)/, "$1 $2").split(' ').map(Number);
-			if(py_version > version) {
-				vscode.window.showErrorMessage(`This extension requires python ${py_version[0]}.${py_version[1]} or higher.`);
-				resolve(false);
-				return;
-			}
-			resolve(true);
+function get_contest_info(contest_name : string, callback : (id : string[], title : string[], label : string[], time : number[], memory : number[]) => void) {
+	let problem_id : string[] = [], problem_title : string[] = [], problem_label : string[] = [], timelimit : number[] = [], memorylimit : number[] = [];
+	https.get(`https://atcoder.jp/contests/${contest_name}/tasks`, (res : any) => {
+		if(res.statusCode !== 200) return;
+		let html = '';
+		res.on('data', (d : Uint8Array) => {
+			const txt = new TextDecoder().decode(d);
+			html += txt.replaceAll('	', '').replaceAll('\n', '');
+		}).on('end', () => {
+			const url = `/contests/${contest_name}/tasks/`;
+			const re = `<td class="text-center no-break"><a href="${url}(.*?)">(.*?)</a></td><td><a href="${url}.*?">(.*?)</a></td><td class="text-right">(.*?)</td><td class="text-right">(.*?)</td>`;
+			const arr = html.match(new RegExp(re.replaceAll('(.*?)', '.*?'), 'g'));
+			if(arr === null) return;
+			arr.forEach(str => {
+				const s = str.replace(new RegExp(re, 'g'), '$1@@$2@@$3@@$4@@$5').split('@@');
+				problem_id.push(s[0]);
+				problem_title.push(s[2]);
+				problem_label.push(s[1]);
+				timelimit.push(parseFloat(s[3]) * (s[3].includes('sec') ? 1000 : 1));
+				memorylimit.push(parseFloat(s[4]) * (s[4].includes('MB') ? 1000 : 1));
+			});
 		});
-	});
-}
-function check_oj_version() {
-	return new Promise((resolve) => {
-		child_process.exec('oj --version', (error, stdout, stderr) => {
-			if(error) {
-				vscode.window.showErrorMessage('Please install online-judge-tools.');
-				resolve(false);
-				return;
+	}).on('close', async () => {
+		if(problem_id.length === 0) {
+			let n = 0;
+			let s = 0;
+			let f2 = false;
+			if (/abc[0-9]{3}/.test(contest_name)) {
+				let abc = Number(contest_name.substring(3));
+				if(abc <= 125) n = 4;
+				else if(abc <= 211) n = 6;
+				else if(abc <= 318) n = 8;
+				else n = 7;
+			} else if (/arc[0-9]{3}/.test(contest_name)) {
+				let arc = Number(contest_name.substring(3));
+				if(arc <= 57) n = 4;
+				else {
+					n = 6;
+					if(arc <= 104) s = 2;
+					if(arc === 120) f2 = true;
+				}
+			} else if (/agc[0-9]{3}/.test(contest_name)) {
+				let agc = Number(contest_name.substring(3));
+				n = 6;
+				if(agc === 28) f2 = true;
 			}
-			console.log();
-			const version = stdout.toString().replace(/online-judge-tools (\d+)\.(\d+)\.(\d+).*/, "$1 $2 $3").split(' ').map(Number);
-			if(oj_version > version) {
-				vscode.window.showErrorMessage(`This extension requires online-judge-tools ${oj_version[0]}.${oj_version[1]}.${oj_version[2]} or higher.`);
-				resolve(false);
-				return;
+			for (let i = s; i < n; ++i)
+				problem_id.push(String.fromCharCode('a'.charCodeAt(0) + i));
+			if (f2) problem_id.push('f2');
+			for (let i = 0; i < problem_id.length; ++i) {
+				problem_title.push('unknown');
+				problem_label.push(problem_id[i].toUpperCase());
+				problem_id[i] = contest_name.replaceAll('-', '_') + '_' + problem_id[i];
+				timelimit.push(2000);
+				memorylimit.push(1024000);
 			}
-			resolve(true);
-		});
+		}
+		callback(problem_id, problem_title, problem_label, timelimit, memorylimit);
 	});
-}
-async function check_py_oj_version() {
-	const [py_ok, oj_ok] = await Promise.all([check_py_version(), check_oj_version()]);
-	return py_ok && oj_ok;
 }
 
 function add_problem(folder: vscode.Uri, contest_name: string, name: string, filename: string, template_uri: vscode.Uri | undefined) {
 	const fullpath = vscode.Uri.joinPath(folder, name);
 	vscode.workspace.fs.createDirectory(fullpath);
 	const filepath = vscode.Uri.joinPath(fullpath, filename);
-	const title = contest_name.replaceAll('-', '_') + '_' + name;
+	const id = contest_name.replaceAll('-', '_') + '_' + name;
 	const jsonstr =
 `{
-    "title": "${title}",
-    "url": "https://atcoder.jp/contests/${contest_name}/tasks/${title}"
+    "title": "${id}",
+    "url": "https://atcoder.jp/contests/${contest_name}/tasks/${id}"
 }\n`;
 	vscode.workspace.fs.writeFile(vscode.Uri.joinPath(fullpath, 'problem/info.json'), (new TextEncoder).encode(jsonstr));
 	if(template_uri != undefined) vscode.workspace.fs.copy(template_uri, filepath);
@@ -110,7 +128,11 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.window.showErrorMessage('The contest name is empty.');
 			return;
 		}
-		let problem_name : string[] = [];
+		get_contest_info(contest_name, (problem_id, problem_title, problem_label, timelimit, memorylimit) => {
+			console.log(problem_id, problem_title, problem_label, timelimit, memorylimit);
+		});
+		/*
+		let problem_id : string[] = [];
 		const contest_name_ub = contest_name.replaceAll('-', '_');
 		https.get(`https://atcoder.jp/contests/${contest_name}/tasks`, (res : any) => {
 			if(res.statusCode !== 200) return;
@@ -119,10 +141,10 @@ export function activate(context: vscode.ExtensionContext) {
 				const re = `<td><a href="/contests/${contest_name}/tasks/${contest_name_ub}_.*"`;
 				const arr = html.match(new RegExp(re, 'g'));
 				if(arr === null) return;
-				arr.forEach(str => problem_name.push(str.slice(re.length - 3, -1)));
+				arr.forEach(str => problem_id.push(str.slice(re.length - 3, -1)));
 			});
 		}).on('close', async () => {
-			if(problem_name.length === 0) {
+			if(problem_id.length === 0) {
 				let n = 0;
 				let s = 0;
 				let f2 = false;
@@ -157,12 +179,13 @@ export function activate(context: vscode.ExtensionContext) {
 					}
 				}
 				for(let i = s; i < n; ++i)
-					problem_name.push(String.fromCharCode('a'.charCodeAt(0) + i));
-				if(f2) problem_name.push('f2');
+					problem_id.push(String.fromCharCode('a'.charCodeAt(0) + i));
+				if(f2) problem_id.push('f2');
 			}
 			const folder = vscode.Uri.joinPath(target_directory, contest_name);
-			problem_name.forEach(name => add_problem(folder, contest_name, name, filename, template_uri));
+			problem_id.forEach(name => add_problem(folder, contest_name, name, filename, template_uri));
 		});
+		*/
 	});
 	context.subscriptions.push(disposable2);
 
